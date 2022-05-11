a224f84b5853ac3301f6615f711c664d5dcea65c
홍채민 <coals0329@cocen.com>
2022-05-11 14:55:00 +0900

    feat(design-pattern): proxy pattern - ex code 2

--- proxy-start/src/test/java/hello/proxy/pureproxy/proxy/ProxyPatternTest.java
+++ proxy-start/src/test/java/hello/proxy/pureproxy/proxy/ProxyPatternTest.java
@@ -1,5 +1,6 @@
 package hello.proxy.pureproxy.proxy;
 
+import hello.proxy.pureproxy.proxy.code.CacheProxy;
 import hello.proxy.pureproxy.proxy.code.ProxyPatternClient;
 import hello.proxy.pureproxy.proxy.code.RealSubject;
 import org.junit.jupiter.api.Test;
@@ -13,6 +14,26 @@ public class ProxyPatternTest {
         client.execute();
         client.execute();
         client.execute();
+    }
+
+    @Test
+    void cacheProxyTest() {
+        RealSubject realSubject = new RealSubject();
+        CacheProxy cacheProxy = new CacheProxy(realSubject);
+        ProxyPatternClient client = new ProxyPatternClient(cacheProxy);
+        client.execute();
+        client.execute();
+        client.execute();
+
+        // 실행 결과
+        // CacheProxy - 프록시 호출
+        // RealSubject - 실제 객체 호출
+        // CacheProxy - 프록시 호출
+        // CacheProxy - 프록시 호출
+
+        // 1. client의 cacheProxy 호출 cacheProxy에 캐시 값이 없다. -> realSubject를 호출, 결과를 캐시에 저장 (1초)
+        // 2. client의 cacheProxy 호출 cacheProxy에 캐시 값이 있다. -> cacheProxy에서 즉시 반환 (0초)
+        // 3. client의 cacheProxy 호출 cacheProxy에 캐시 값이 있다. -> cacheProxy에서 즉시 반환 (0초)
     }
 
 }
