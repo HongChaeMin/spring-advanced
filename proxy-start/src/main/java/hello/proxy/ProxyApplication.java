64459f2c1d548fc09cd63029260ac6157b2253c3
minHong <ghdcoalss33@gmail.com>
2022-05-16 16:28:29 +0900

    feat(proxy-factory): proxy factory - apply 1

--- proxy-start/src/main/java/hello/proxy/ProxyApplication.java
+++ proxy-start/src/main/java/hello/proxy/ProxyApplication.java
@@ -8,6 +8,7 @@ import hello.proxy.config.v2_dynamicproxy.DynamicProxyBasicConfig;
 import hello.proxy.config.v2_dynamicproxy.DynamicProxyFilterConfig;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
+import hello.proxy.v3_proxyfactory.ProxyFactoryConfigV1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -15,7 +16,7 @@ import org.springframework.context.annotation.Import;
 
 // @Import(AppV1Config.class) // 클래스를 스프링 빈으로 등록
 // @Import({AppV1Config.class, AppV2Config.class})
-@Import(DynamicProxyFilterConfig.class)
+@Import(ProxyFactoryConfigV1.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의, @ComponentScan 의 기능과 같다 해당 패키지와 그 하위 패키지를 컴포넌트 스캔
 public class ProxyApplication {
 
