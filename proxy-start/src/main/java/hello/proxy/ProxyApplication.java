3991a9cb62fc7934960d474ab249b652d307ff9e
minHong <ghdcoalss33@gmail.com>
2022-05-16 16:51:57 +0900

    feat(file): v3_proxyfactory package move

--- proxy-start/src/main/java/hello/proxy/ProxyApplication.java
+++ proxy-start/src/main/java/hello/proxy/ProxyApplication.java
@@ -1,15 +1,8 @@
 package hello.proxy;
 
-import hello.proxy.config.AppV1Config;
-import hello.proxy.config.AppV2Config;
-import hello.proxy.config.v1_proxy.ConcreteProxyConfig;
-import hello.proxy.config.v1_proxy.InterfaceProxyConfig;
-import hello.proxy.config.v2_dynamicproxy.DynamicProxyBasicConfig;
-import hello.proxy.config.v2_dynamicproxy.DynamicProxyFilterConfig;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
-import hello.proxy.v3_proxyfactory.ProxyFactoryConfigV1;
-import hello.proxy.v3_proxyfactory.ProxyFactoryConfigV2;
+import hello.proxy.config.v3_proxyfactory.ProxyFactoryConfigV2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
