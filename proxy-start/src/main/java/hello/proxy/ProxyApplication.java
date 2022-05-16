03f40632580b55a1d92ae39c8edfeaf0e19e3e9a
minHong <ghdcoalss33@gmail.com>
2022-05-16 11:04:54 +0900

    feat(design-patter): JDK 동적 proxy - 적용1

--- proxy-start/src/main/java/hello/proxy/ProxyApplication.java
+++ proxy-start/src/main/java/hello/proxy/ProxyApplication.java
@@ -4,6 +4,7 @@ import hello.proxy.config.AppV1Config;
 import hello.proxy.config.AppV2Config;
 import hello.proxy.config.v1_proxy.ConcreteProxyConfig;
 import hello.proxy.config.v1_proxy.InterfaceProxyConfig;
+import hello.proxy.config.v2_dynamicproxy.DynamicProxyBasicConfig;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -13,7 +14,7 @@ import org.springframework.context.annotation.Import;
 
 // @Import(AppV1Config.class) // 클래스를 스프링 빈으로 등록
 // @Import({AppV1Config.class, AppV2Config.class})
-@Import(ConcreteProxyConfig.class)
+@Import(DynamicProxyBasicConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의, @ComponentScan 의 기능과 같다 해당 패키지와 그 하위 패키지를 컴포넌트 스캔
 public class ProxyApplication {
 
