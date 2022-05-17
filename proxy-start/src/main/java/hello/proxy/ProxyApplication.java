f238f2d66180951989c26bddccae0104fb703461
minHong <ghdcoalss33@gmail.com>
2022-05-17 10:15:09 +0900

    feat(bean-post-processor): bean post processor provided by spring 1

--- proxy-start/src/main/java/hello/proxy/ProxyApplication.java
+++ proxy-start/src/main/java/hello/proxy/ProxyApplication.java
@@ -1,6 +1,7 @@
 package hello.proxy;
 
 import hello.proxy.config.v4_postprocessor.BeanPostProcessorConfig;
+import hello.proxy.config.v5_autoproxy.AutoProxyConfig;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import hello.proxy.config.v3_proxyfactory.ProxyFactoryConfigV2;
@@ -11,7 +12,7 @@ import org.springframework.context.annotation.Import;
 
 // @Import(AppV1Config.class) // 클래스를 스프링 빈으로 등록
 // @Import({AppV1Config.class, AppV2Config.class})
-@Import(BeanPostProcessorConfig.class)
+@Import(AutoProxyConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의, @ComponentScan 의 기능과 같다 해당 패키지와 그 하위 패키지를 컴포넌트 스캔
 public class ProxyApplication {
 
