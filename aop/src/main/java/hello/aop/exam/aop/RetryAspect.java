8efc82654c492d515587ca8a46f0cdb214f669e9
minHong <ghdcoalss33@gmail.com>
2022-05-20 12:11:27 +0900

    spring-aop(example): retry AOP

---
+++ aop/src/main/java/hello/aop/exam/aop/RetryAspect.java
@@ -0,0 +1,35 @@
+package hello.aop.exam.aop;
+
+import hello.aop.exam.annotation.Retry;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+
+import java.util.Objects;
+
+@Slf4j
+@Aspect
+public class RetryAspect {
+
+    @Around("@annotation(retry)")
+    // @annotation(retry) , Retry retry 를 사용해서 어드바이스에 애노테이션을 파라미터로 전달
+    public Object doRetry(ProceedingJoinPoint joinPoint, Retry retry) throws Throwable {
+        log.info("[retry] {} retry : {}", joinPoint.getSignature(), retry);
+
+        int maxRetry = retry.value();
+        Exception exceptionHolder = null;
+
+        for (int retryCount = 1; retryCount <= maxRetry; retryCount++) {
+            try {
+                log.info("[retry] try count : {} / {}", retryCount, maxRetry);
+                return joinPoint.proceed();
+            } catch (Exception e) {
+                exceptionHolder = e;
+            }
+        }
+        throw Objects.requireNonNull(exceptionHolder);
+    }
+
+}
